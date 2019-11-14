91a7a8c1aff35f91fcef0d07b3a477effdd1a2bf
gatomei <atomeigeorgiana@yahoo.com>
2019-11-14 17:29:43 +0200

    image converted

--- HGR/HGR/Source.cpp
+++ HGR/HGR/Source.cpp
@@ -1,1 +1,25 @@
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+int main()
+{
+	Mat image, greyimg;
+	char *imagename = (char*)"gesture.jpg";
+	image = imread(imagename, CV_LOAD_IMAGE_COLOR);   // Read the file
+
+	if (!image.data)                              // Check for invalid input
+	{
+		std::cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+	cvtColor(image, greyimg, COLOR_BGR2GRAY);
+
+	namedWindow("Display window", WINDOW_AUTOSIZE);// Create a window for display.
+	//imshow("Display window", image);   // Show our image inside it.
+	imshow("Display window", greyimg);
+	waitKey(0);                                          // Wait for a keystroke in the window
+	return 0;
+}
